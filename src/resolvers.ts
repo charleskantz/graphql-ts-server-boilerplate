0ab0f6f75df9e229cefa90f95559e5ba1cea5d92
Charles Kantz <kantz.charles@gmail.com>
2021-02-18 16:54:33 -0800

    added bcrypt for pw hashing. register() now adds user to db

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,7 @@
 import { ResolverMap } from "./types/graphql-utils";
 import { GQL } from "./types/schema";
+import * as bcrypt from "bcryptjs";
+import { User } from "./entity/User";
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -7,8 +9,17 @@ export const resolvers: ResolverMap = {
       `Hello ${name || "World"}`,
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
-      return email + password;
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = User.create({
+        email,
+        password: hashedPassword,
+      });
+      await user.save();
+      return true;
     },
   },
 };
